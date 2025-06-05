75311bbb6ce80257cb24eeb38277c259bf54a43a
Thapson Nyirenda <thapsonknyirenda@gmail.com>
2025-06-05 08:24:13 +0200

    Added some JWT functions

--- app/auth/jwt_handler.py
+++ app/auth/jwt_handler.py
@@ -1,9 +1,23 @@
-# This file is responsible for signing, decoding, encoding, returning JWT tokens, and verifying them.
-
-
 import time
 import jwt
 from decouple import config
 
 JWT_SECRET= config("secret")
 JWT_ALGORITHM = config("algorithm")
+
+# Function to return the generated token
+def token_response(token:str):
+    return{
+        "access_token": token,
+        "token_type": "bearer"
+    }
+
+# Function for Signing the JWT token
+def signJWT(user_id: str):
+    payload={
+        "user_id": user_id,
+        "expires": time.time() + 3600
+    }
+    token = jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
+    return token_response(token)
+
